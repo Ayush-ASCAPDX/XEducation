5b80e19c34f4eb90a4206bec95254daaebc40d61
Ayush-ASCAPDX <zigar1499@gmail.com>
2026-02-23 10:32:55 +0530

    Delete ecommerce-app directory

--- ecommerce-app/routes/adminRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { protect, adminOnly } = require("../middlewares/authMiddleware");
-const { getDashboardStats } = require("../controllers/adminController");
-
-const router = express.Router();
-
-router.get("/dashboard", protect, adminOnly, getDashboardStats);
-
-module.exports = router;
